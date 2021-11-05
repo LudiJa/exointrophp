9cff7b8f67af4ff2b61280ec4b5eeadf532becf9
Ludivine <lud.jamin@gmail.com>
2021-11-05 09:46:28 +0100

    11

---
+++ exo11/index.php
@@ -0,0 +1,16 @@
+<!-- 347872 / 3600 = h
+h / 60 = m
+m / 60 = s 
+347872s équivaut à 96h37m52s -->
+
+<?php
+
+function heure($seconde) {
+    $heure = (floor($seconde / 3600));
+    $rest = $seconde % 3600;
+    $min = (floor($rest / 60));
+    $rest = $rest % 60;
+    echo "L'heure est $heure h $min min $rest s";
+};
+
+echo heure(347872);
